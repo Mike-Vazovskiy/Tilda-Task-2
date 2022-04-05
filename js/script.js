5817cc64ad9aaf19ee48e9a3032c296d9b1efa6a
Михаил Калмыков <68296867+Mike-Vazovskiy@users.noreply.github.com>
2022-04-05 12:41:25 +0300

    Removed caption for modal

--- js/script.js
+++ js/script.js
@@ -185,11 +185,9 @@ setTimeout(() => {
   var modal = document.getElementById('modal');
   var img = document.getElementById('largeImg');
   var modalImg = document.getElementById("img01");
-  var captionText = document.getElementById("caption");
   img.onclick = function(){
       modal.style.display = "block";
       modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
   }
 
   var span = document.getElementsByClassName("close")[0];
